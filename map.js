e85bea01ea8014122955cfce082ea65c6cd1f115
ryan atienza <ratienza@ucsd.edu>
2026-05-15 22:01:14 -0700

    bug fix

--- map.js
+++ map.js
@@ -20,7 +20,7 @@ const map = new mapboxgl.Map({
 const svg = d3.select('#map').select('svg');
 
 function getCoords(station) {
-  const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+  const point = new mapboxgl.LngLat(+station.lon, +station.lat);
   const { x, y } = map.project(point);
   return { cx: x, cy: y };
 }
